4946536412d00bc438bc45af9f886f86d5571afa
jerrybennett <jerrysbusiness@gmail.com>
2018-04-27 16:34:57 -0400

    add update to ideas_controller

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -7,6 +7,11 @@ class Api::V1::IdeasController < ApplicationController
     @idea = Idea.create(idea_params)
     render json: @idea
   end
+  def update
+    @idea = Idea.find(params[:id])
+    @idea.update_attributes(idea_params)
+    render json: @idea
+  end
   private
     def idea_params
       params.require(:idea).permit(:title, :body)
